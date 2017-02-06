8a700da7804ddda8606f2e7295286508e54b4767
Rohit Kumar <rohit.kumard95@gmail.com>
2017-02-07 01:03:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -51,10 +51,10 @@ var articles={
 };
 
 function createTemplate(data){
-    var title=data.title;
-    var date=data.date;
-    var heading=data.heading;
-    var content=data.content;
+var title=data.title;
+var date=data.date;
+var heading=data.heading;
+var content=data.content;
 
 var htmlTemplate=`
 <html>
@@ -92,7 +92,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function(req,res){
-    res.send(createTemplate(articles[articleName]));
+res.send(createTemplate(articles[articleName]));
 });
 var articleName=req.params.articleName;
 app.get('/ui/style.css', function (req, res) {
