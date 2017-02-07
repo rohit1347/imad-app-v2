fa03dcc81a56d66c5b6e4f8d2a7284a72a0f7527
Rohit Kumar <rohit.kumard95@gmail.com>
2017-02-08 02:29:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,11 @@ console.log('Loaded!');
 var element= document.getElementById('main-text');
 element.innerHTML='New Value';
 var img=document.getElementById('madi');
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onClick=function(){
+    var interval=setInterval(moveRight,100);
   img.style.marginleft='100px'; 
 };
