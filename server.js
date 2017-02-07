e479c0a9b5a085d19afd06aa22568a214b44c604
Rohit Kumar <rohit.kumard95@gmail.com>
2017-02-07 23:51:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,10 +91,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var articleName=req.params.articleName;
 app.get('/:articleName', function(req,res){
 res.send(createTemplate(articles[articleName]));
 });
-var articleName=req.params.articleName;
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
