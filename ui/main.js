a679f0121899180a5df3da6e2d9d33f85508f0ab
Rohit Kumar <rohit.kumard95@gmail.com>
2017-02-10 00:41:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+/* console.log('Loaded!');
 var element= document.getElementById('main-text');
 element.innerHTML='New Value';
 var img=document.getElementById('madi');
@@ -9,4 +9,17 @@ function moveRight(){
 img.onClick=function(){
     var interval=setInterval(moveRight,100);
   img.style.marginleft='100px'; 
+};*/
+
+//Counter Code
+var button=document.getElementById('counter');
+var counter=0;
+button.onclick=function(){
+    
+    //Make a request to the counter endpoint
+    //Capture the request and store it in a variable
+    //Render the variable in the correct span
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
 };
