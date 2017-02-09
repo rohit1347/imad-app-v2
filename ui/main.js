c8b14bb70f9b64381f67cd14d903b16cf78b1e2d
Rohit Kumar <rohit.kumard95@gmail.com>
2017-02-10 01:00:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,7 +23,7 @@ request.onreadystatechange=function(){
     if(request.readyState===XMLHttpRequest.DONE){
         if(request.status===200){
             var counter= request.responseText;
-            var span=document.getElementById('count');
+            var span=document.getElementById("count");
             span.innerHTML=counter.toString();
         }
     }
