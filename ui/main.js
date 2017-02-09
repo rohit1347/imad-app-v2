e0811c98a40222274bf49aae26da7d7b924a8e8f
Rohit Kumar <rohit.kumard95@gmail.com>
2017-02-10 00:50:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,13 +13,27 @@ img.onClick=function(){
 
 //Counter Code
 var button=document.getElementById('counter');
-var counter=0;
+// var counter=0;
 button.onclick=function(){
     
     //Make a request to the counter endpoint
+var request=new XMLHttpRequest();
     //Capture the request and store it in a variable
+request.onreadystatechange=fucntion(){
+    if(request.readyState===XMLHttpRequest.DONE){
+        if(request.status===200){
+            var counter= request.responseText;
+            span.innerHTML=counter.toString();
+        }
+    }
+
+};
+request.open('GET', 'http://rohit1347.imad.hasura-app.imad.io/counter',true);
+request.send(null);
+    
+    
     //Render the variable in the correct span
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+    // counter=counter+1;
+    // var span=document.getElementById('count');
+    
 };
